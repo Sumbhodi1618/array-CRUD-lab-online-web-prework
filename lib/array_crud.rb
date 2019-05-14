c19aacfe2960820728a27a7469d502caf6283d2b
Justin <sumbhodi1618@gmail.com>
2019-05-14 21:47:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,11 +12,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.shift = "Data"
+  create_an_array.unshift = "Data"
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop = "Riker"
+  create_an_array.shift = "Picard"
 end
 
 def remove_element_from_start_of_array(array)
